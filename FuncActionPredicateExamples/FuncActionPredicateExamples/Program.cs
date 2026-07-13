3dfc26b57e84a2e64f012abd50c29ebc6f3579b8
kev <kev@daryus.com>
2026-07-13 13:58:39 +0100

    Part 7a - Delegates - Func, Action and Predicate

---
+++ FuncActionPredicateExamples/FuncActionPredicateExamples/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FuncActionPredicateExamples
+{
+    class Program //20260710 Part 7 - Delegates - Fund, Action and Predicate
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

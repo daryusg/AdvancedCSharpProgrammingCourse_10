1d05a227c8f70aa4297078a9eeeece5dcf277334
kev <kev@daryus.com>
2026-09-04 23:51:11 +0100

    Part 17 - Async / Await Task - Introduction

---
+++ TestClientAsyncUWP/TestClientAsyncUWP/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System.Net.Http;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace TestClientAsyncUWP
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        int _localOperationCounter = 0;
+        int _webAPIOperationCounter = 0;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+      private void AddListItem(string text)
+        {
+            ListViewItem listViewItem = new ListViewItem();
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = text;
+            listViewItem.Content = textBlock;
+            lvwOutput.Items.Add(listViewItem);
+
+        }
+
+        private void btnLocalOperation_Click(object sender, RoutedEventArgs e)
+        {
+            _localOperationCounter++;
+            AddListItem($"Fast Local Operation Completed {_localOperationCounter}");
+        }
+
+        private async void btnWebAPICall_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync("https://localhost:7133/TestLongOperation");
+
+            string result = await httpResponseMessage.Content.ReadAsStringAsync();
+
+            _webAPIOperationCounter++;
+
+            AddListItem($"{result} {_webAPIOperationCounter}");
+        }
+    }
+}
---
+++ TestTimeConsumingOperationWebAPI/TestTimeConsumingOperationWebAPI/Controllers/TestLongOperationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestTimeConsumingOperationWebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestLongOperationController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            // Simulate a long-running operation
+            int secondsDelay = 5;
+            await Task.Delay(secondsDelay * 1000);
+            return Ok("Web API long operation completed successfully.");
+        }
+    }
+}
